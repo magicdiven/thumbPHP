3da4e4c813ebc1f2d48873efb87ecb7de5ad4ed6
magicdiven <apple@apple.local>
2017-02-17 10:09:22 +0800

    删除测试代码
    RT

--- Application/Index/Controller/IndexController.php
+++ Application/Index/Controller/IndexController.php
@@ -16,34 +16,5 @@ class IndexController extends Controller
     {
         $this->display();
     }
-
-    public function test()
-    {
-        $arr = array(
-            1,
-            3,
-            1,
-            2,
-            array(
-                1,
-                1,
-                array(
-                    1,2,3,1
-                )
-            ),
-            array(
-                1,1
-            ),
-            array(
-                2
-            ),
-            array(
-                2,2
-            ),
-        );//4 6 8出现1次,其他出现2次
-        echo '<pre>';
-        print_r(multiArrUnique(multiArrUnique($arr,TRUE)));
-    }
 }
 
-?>
